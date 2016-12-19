4817dd9e0998387fb9f031c2f43c5835ba6b737e
marcosmohar <elcabeca@gmail.com>
2016-12-19 16:16:57 -0600

    added .babelrc and changed imports on both buildScript, adapted package.json scripts to use babel-node

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
 
 // conseguir el path de index.html
 app.get('/', function(req, res){
